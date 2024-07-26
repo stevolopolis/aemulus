fb69edb6dc7e33014d6f24c6bf5e98ca01c4e4e2
steven <stevenluots@gmail.com>
2024-07-26 18:33:09 -0400

    add cors to backend js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 let notes = [
@@ -24,6 +25,7 @@ let notes = [
 
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
